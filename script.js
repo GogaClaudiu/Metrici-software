e337d30643a240f1e593c20a06cf1b99f0e4be34
GogaClaudiu <claudiug302000@gmail.com>
2024-11-09 19:06:42 +0200

    enrich with new features

--- script.js
+++ script.js
@@ -7,52 +7,11 @@
 /////////////////////////////////////////////////
 // Data
 
-// DIFFERENT DATA! Contains movement dates, currency and locale
 
-const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
-
-  movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-07-26T17:01:17.194Z',
-    '2020-07-28T23:36:17.929Z',
-    '2020-08-01T10:51:36.790Z',
-  ],
-  currency: 'EUR',
-  locale: 'pt-PT', // de-DE
-};
-
-const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
-
-  movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
-  ],
-  currency: 'USD',
-  locale: 'en-US',
-};
-
-const accounts = [account1, account2];
+const accounts = [];
 
 /////////////////////////////////////////////////
-// Elements
+// Elemente dinamice
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
 const labelBalance = document.querySelector('.balance__value');
@@ -60,7 +19,6 @@ const labelSumIn = document.querySelector('.summary__value--in');
 const labelSumOut = document.querySelector('.summary__value--out');
 const labelSumInterest = document.querySelector('.summary__value--interest');
 const labelTimer = document.querySelector('.timer');
-
 const containerApp = document.querySelector('.app');
 const containerMovements = document.querySelector('.movements');
 
@@ -70,6 +28,7 @@ const btnLoan = document.querySelector('.form__btn--loan');
 const btnClose = document.querySelector('.form__btn--close');
 const btnSort = document.querySelector('.btn--sort');
 
+const btTransferMyAccoutns = document.querySelector('.button-transfer-my-accounts');
 const inputLoginUsername = document.querySelector('.login__input--user');
 const inputLoginPin = document.querySelector('.login__input--pin');
 const inputTransferTo = document.querySelector('.form__input--to');
@@ -77,89 +36,150 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+document.addEventListener('DOMContentLoaded', function() {});
 
-/////////////////////////////////////////////////
-// Functions
+const logOut = document.querySelector('#log-out');
+logOut.addEventListener('click', function () {
+  location.reload();
+  localStorage.clear();
+});
+const contextAccount = null;
 
-const formatMovementDate = function (date, locale) {
-  const calcDaysPassed = (date1, date2) =>
-    Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+let cachedData = null;
 
-  const daysPassed = calcDaysPassed(new Date(), date);
-  console.log(daysPassed);
-
-  if (daysPassed === 0) return 'Today';
-  if (daysPassed === 1) return 'Yesterday';
-  if (daysPassed <= 7) return `${daysPassed} days ago`;
-
-  // const day = `${date.getDate()}`.padStart(2, 0);
-  // const month = `${date.getMonth() + 1}`.padStart(2, 0);
-  // const year = date.getFullYear();
-  // return `${day}/${month}/${year}`;
-  return new Intl.DateTimeFormat(locale).format(date);
-};
-
-const formatCur = function (value, locale, currency) {
-  return new Intl.NumberFormat(locale, {
-    style: 'currency',
-    currency: currency,
-  }).format(value);
-};
-
-const displayMovements = function (acc, sort = false) {
+const displayMovements = async function (clientID, sort = false) {
   containerMovements.innerHTML = '';
 
-  const movs = sort
-    ? acc.movements.slice().sort((a, b) => a - b)
-    : acc.movements;
+  try {
+    const data = await fetchMovements(clientID);
+    processAndDisplayData(data, sort);
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+};
 
-  movs.forEach(function (mov, i) {
-    const type = mov > 0 ? 'deposit' : 'withdrawal';
+// Funcție de preluare a mișcărilor de pe server
+async function fetchMovements(clientID) {
+  const response = await fetch(`http://localhost:3000/transactions/${clientID}`);
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  return response.json();
+}
 
-    const date = new Date(acc.movementsDates[i]);
-    const displayDate = formatMovementDate(date, acc.locale);
+// Funcție de procesare și afișare a datelor
+function processAndDisplayData(apiResponse, sort) {
+  const sortedData = sort ?
+    apiResponse.slice().sort((a, b) => parseFloat(a.transactionAmount) - parseFloat(b.transactionAmount))
+    : apiResponse;
 
-    const formattedMov = formatCur(mov, acc.locale, acc.currency);
-
+  sortedData.forEach(function (transaction, i) {
+    const type = parseFloat(transaction.transactionAmount) > 0 ? 'deposit' : 'withdrawal';
     const html = `
       <div class="movements__row">
-        <div class="movements__type movements__type--${type}">${
-      i + 1
-    } ${type}</div>
-        <div class="movements__date">${displayDate}</div>
-        <div class="movements__value">${formattedMov}</div>
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__value">${transaction.transactionAmount}€</div>
       </div>
     `;
 
     containerMovements.insertAdjacentHTML('afterbegin', html);
   });
-};
+}
+// eveniment afaugat pe butonul de transfer (conturi propriu)
+btTransferMyAccoutns.addEventListener('click',  function (e) {
+  e.preventDefault();
+  const inputToTransfer = document.querySelector('.my_accounts_options').value;
+  console.log(inputToTransfer)
+  const inputValueForAmount = document.querySelector('.form_input_my_accounts-ammount').value;
+console.log(inputValueForAmount);
+  try {
+    const currentAccount = JSON.parse(localStorage.getItem('currentAccount'));
+    const destinationAccountCode = inputToTransfer;
+    const amount = inputValueForAmount;
 
-const calcDisplayBalance = function (acc) {
-  acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = formatCur(acc.balance, acc.locale, acc.currency);
+    const url = `http://localhost:3000/transfer/${currentAccount.accountCode}/${destinationAccountCode}/${amount}`;
+    fetchTransfer(url)
+    .then(() => updateBalanceAfterDelay(currentAccount.accountCode))
+    .then(() => console.log('Transfer completed successfully'))
+.then(()=>inputValueForAmount.value = 0)
+    .catch(error => console.error('There was a problem with the fetch operation:', error.message));
+    
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error.message);
+  }
+});
+
+
+// buton pentru a se declansa evenimentul pentru a transfera banii (in orice cont strain)
+btnTransfer.addEventListener('click',  function (e) {
+  e.preventDefault();
+  const inputToTransfer = document.querySelector('.form__input.form__input--to').value;
+  const inputValueForAmount = document.querySelector('.form__input.form__input--amount').value;
+
+  try {
+    const currentAccount = JSON.parse(localStorage.getItem('currentAccount'));
+    const destinationAccountCode = inputToTransfer;
+    const amount = inputValueForAmount;
+
+    const url = `http://localhost:3000/transfer/${currentAccount.accountCode}/${destinationAccountCode}/${amount}`;
+    fetchTransfer(url)
+    .then(() => updateBalanceAfterDelay(currentAccount.accountCode))
+    .then(() => console.log('Transfer completed successfully'))
+    .catch(error => console.error('There was a problem with the fetch operation:', error.message));
+    
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error.message);
+  }
+});
+
+
+// Function pentru a se rwliza transferul (se apeleaza un API )
+async function fetchTransfer(url) {
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+
+  return response.json();
+}
+
+// Funcție de actualizare a soldului 
+async function updateBalanceAfterDelay(accountCode) {
+  // Wait for 3 seconds (3000 milliseconds)
+  await new Promise(resolve => setTimeout(resolve, 3000));
+
+  const balanceData = await fetchBalance(accountCode);
+  labelBalance.textContent = `${balanceData[0].balance}€`;
+  displayMovements(currentAccount.clientID, false);
+}
+
+const calcDisplayBalance = function (balance) {
+  labelBalance.textContent = `${balance}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = formatCur(incomes, acc.locale, acc.currency);
+  labelSumIn.textContent = `${incomes}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = formatCur(Math.abs(out), acc.locale, acc.currency);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
     .map(deposit => (deposit * acc.interestRate) / 100)
     .filter((int, i, arr) => {
-      // console.log(arr);
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = formatCur(interest, acc.locale, acc.currency);
+  labelSumInterest.textContent = `${interest}€`;
 };
 
 const createUsernames = function (accs) {
@@ -173,176 +193,95 @@ const createUsernames = function (accs) {
 };
 createUsernames(accounts);
 
-const updateUI = function (acc) {
-  // Display movements
-  displayMovements(acc);
 
-  // Display balance
-  calcDisplayBalance(acc);
-
-  // Display summary
-  calcDisplaySummary(acc);
-};
-
-const startLogOutTimer = function () {
-  const tick = function () {
-    const min = String(Math.trunc(time / 60)).padStart(2, 0);
-    const sec = String(time % 60).padStart(2, 0);
-
-    // In each call, print the remaining time to UI
-    labelTimer.textContent = `${min}:${sec}`;
-
-    // When 0 seconds, stop timer and log out user
-    if (time === 0) {
-      clearInterval(timer);
-      labelWelcome.textContent = 'Log in to get started';
-      containerApp.style.opacity = 0;
-    }
-
-    // Decrease 1s
-    time--;
-  };
-
-  // Set time to 5 minutes
-  let time = 120;
-
-  // Call the timer every second
-  tick();
-  const timer = setInterval(tick, 1000);
-
-  return timer;
-};
-
-///////////////////////////////////////
 // Event handlers
-let currentAccount, timer;
+let currentAccount;
+let cachedDataAccount;
+// Partea de logare 
+const fetchAccount = async (username, pin) => {
+  const url = `http://localhost:3000/login?accountCode=${username}&pin=${pin}`;
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('Failed to fetch account');
+  return response.json();
+};
 
-// FAKE ALWAYS LOGGED IN
-// currentAccount = account1;
-// updateUI(currentAccount);
-// containerApp.style.opacity = 100;
+const fetchBalance = async (clientID) => {
+  const url = `http://localhost:3000/balance/${clientID}`;
+  const response = await fetch(url);
+  if (!response.ok) throw new Error('Failed to fetch balance');
+  return response.json();
+};
+// Update fereastra UI
+const updateUI = (account, balance) => {
+  account.owner = `${account.firstName} ${account.lastName}`;
+  account.interestRate = 1.5;
 
+  labelWelcome.textContent = `Welcome back, ${account.owner}`;
+  containerApp.style.opacity = 100;
+  calcDisplayBalance(balance);
+  displayMovements(account.clientID, false);
+
+  inputLoginUsername.value = inputLoginPin.value = '';
+  inputLoginPin.blur();
+};
+
+// Functie pentru a se realiza login
 btnLogin.addEventListener('click', function (e) {
-  // Prevent form from submitting
+  localStorage.clear();
   e.preventDefault();
 
-  currentAccount = accounts.find(
-    acc => acc.username === inputLoginUsername.value
-  );
-  console.log(currentAccount);
+  let accountData; 
 
-  if (currentAccount?.pin === +inputLoginPin.value) {
-    // Display UI and message
-    labelWelcome.textContent = `Welcome back, ${
-      currentAccount.owner.split(' ')[0]
-    }`;
-    containerApp.style.opacity = 100;
+  //salvare date despre cont 
+  fetchAccount(inputLoginUsername.value, inputLoginPin.value)
+    .then(data => {
+      accountData = data; // Setați accountData în acest moment
+      if (accountData.length > 0) {
+        const currentAccount = accountData[0];
+        return fetchBalance(currentAccount.accountCode);
+      } else {
+        console.error('Login failed: Invalid account code or pin');
+        // Gestionați eșecul de conectare, poate afișând un mesaj de eroare utilizatorului
+        return Promise.reject(new Error('Invalid account code or pin'));
+      }
+    })
+    .then(balanceData => {
+      const currentAccount = JSON.stringify(accountData[0]);
 
-    // Create current date and time
-    const now = new Date();
-    const options = {
-      hour: 'numeric',
-      minute: 'numeric',
-      day: 'numeric',
-      month: 'numeric',
-      year: 'numeric',
-      // weekday: 'long',
-    };
-    // const locale = navigator.language;
-    // console.log(locale);
-
-    labelDate.textContent = new Intl.DateTimeFormat(
-      currentAccount.locale,
-      options
-    ).format(now);
-
-    // const day = `${now.getDate()}`.padStart(2, 0);
-    // const month = `${now.getMonth() + 1}`.padStart(2, 0);
-    // const year = now.getFullYear();
-    // const hour = `${now.getHours()}`.padStart(2, 0);
-    // const min = `${now.getMinutes()}`.padStart(2, 0);
-    // labelDate.textContent = `${day}/${month}/${year}, ${hour}:${min}`;
-
-    // Clear input fields
-    inputLoginUsername.value = inputLoginPin.value = '';
-    inputLoginPin.blur();
-
-    // Timer
-    if (timer) clearInterval(timer);
-    timer = startLogOutTimer();
-
-    // Update UI
-    updateUI(currentAccount);
-  }
+      localStorage.setItem('currentAccount', currentAccount);
+      updateUI(accountData[0], balanceData[0].balance);
+    })
+    .catch(err => {
+      console.error('Error during login:', err);
+    });
 });
+ 
 
-btnTransfer.addEventListener('click', function (e) {
-  e.preventDefault();
-  const amount = +inputTransferAmount.value;
-  const receiverAcc = accounts.find(
-    acc => acc.username === inputTransferTo.value
-  );
-  inputTransferAmount.value = inputTransferTo.value = '';
-
-  if (
-    amount > 0 &&
-    receiverAcc &&
-    currentAccount.balance >= amount &&
-    receiverAcc?.username !== currentAccount.username
-  ) {
-    // Doing the transfer
-    currentAccount.movements.push(-amount);
-    receiverAcc.movements.push(amount);
-
-    // Add transfer date
-    currentAccount.movementsDates.push(new Date().toISOString());
-    receiverAcc.movementsDates.push(new Date().toISOString());
-
-    // Update UI
-    updateUI(currentAccount);
-
-    // Reset timer
-    clearInterval(timer);
-    timer = startLogOutTimer();
-  }
-});
-
-btnLoan.addEventListener('click', function (e) {
+// Function to handle loan
+function handleLoan(e) {
   e.preventDefault();
 
-  const amount = Math.floor(inputLoanAmount.value);
+  const amount = Number(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    setTimeout(function () {
-      // Add movement
-      currentAccount.movements.push(amount);
+    // Adauga tranzactii
+    currentAccount.movements.push(amount);
 
-      // Add loan date
-      currentAccount.movementsDates.push(new Date().toISOString());
-
-      // Update UI
-      updateUI(currentAccount);
-
-      // Reset timer
-      clearInterval(timer);
-      timer = startLogOutTimer();
-    }, 2500);
+    // Update UI
+    updateUI(currentAccount);
   }
   inputLoanAmount.value = '';
-});
+}
 
-btnClose.addEventListener('click', function (e) {
+// Funcție de gestionare a închiderii contului
+function handleCloseAccount(e) {
   e.preventDefault();
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    +inputClosePin.value === currentAccount.pin
+    Number(inputClosePin.value) === currentAccount.pin
   ) {
-    const index = accounts.findIndex(
-      acc => acc.username === currentAccount.username
-    );
-    console.log(index);
-    // .indexOf(23)
+    const index = accounts.findIndex(acc => acc.username === currentAccount.username);
 
     // Delete account
     accounts.splice(index, 1);
@@ -352,11 +291,54 @@ btnClose.addEventListener('click', function (e) {
   }
 
   inputCloseUsername.value = inputClosePin.value = '';
-});
+}
 
-let sorted = false;
-btnSort.addEventListener('click', function (e) {
+// Funcție de gestionare a mișcărilor de sortare
+function handleSortMovements(e) {
   e.preventDefault();
   displayMovements(currentAccount.movements, !sorted);
-  sorted = !sorted;
-});
+
+}
+
+// Event listeners
+btnLoan.addEventListener('click', handleLoan);
+btnClose.addEventListener('click', handleCloseAccount);
+btnSort.addEventListener('click', handleSortMovements);
+
+
+
+
+// 1.
+const bankDepositSum = accounts
+  .flatMap(acc => acc.movements)
+  .filter(mov => mov > 0)
+  .reduce((sum, cur) => sum + cur, 0);
+
+console.log(bankDepositSum);
+
+
+const numDeposits1000 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((count, cur) => (cur >= 1000 ? ++count : count), 0);
+
+console.log(numDeposits1000);
+
+
+
+// 3.
+const { deposits, withdrawals } = accounts
+  .flatMap(acc => acc.movements)
+  .reduce(
+    (sums, cur) => {
+
+      sums[cur > 0 ? 'deposits' : 'withdrawals'] += cur;
+      return sums;
+    },
+    { deposits: 0, withdrawals: 0 }
+  );
+
+console.log(deposits, withdrawals);
+
+
+
+
